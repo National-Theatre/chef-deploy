ff4699074520ba6dbccf492b782d0d3ecca0ca86
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2015-08-18 16:07:16 +0100

    Add magento

--- recipes/magento_deploy.rb
+++ recipes/magento_deploy.rb
@@ -6,3 +6,67 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+magento = {}
+node['nt-deploy']['sites'].each do |site, data|
+  magento[site] = {}
+  magento[site]['repo_path'] = node['nt-deploy']['sites'][site]['repo_path']
+  magento[site]['repo_tag'] = node['nt-deploy']['sites'][site].fetch('repo_tag', false)
+  magento[site]['repo_branch'] = node['nt-deploy']['sites'][site].fetch('repo_branch', 'develop')
+  magento[site]['site_path'] = node['nt-deploy']['sites'][site].fetch('site_path', '/var/www')
+  magento[site]['repo_user'] = node['nt-deploy']['sites'][site].fetch('repo_user', 'git')
+  magento[site]['site_type'] = node['nt-deploy']['sites'][site].fetch('site_type', 'magento')
+  execute 'clone_site' do
+    command "git clone #{magento[site]['repo_user']}@#{site}:#{magento[site]['repo_path']} #{magento[site]['site_path']}/#{site}"
+    not_if { ::File.exists?("#{magento[site]['site_path']}/#{site}") || magento[site]['site_type'] != "magento" }
+  end
+  
+  execute 'checkout_branch' do
+    cwd "#{magento[site]['site_path']}/#{site}"
+    command "git checkout -b #{magento[site]['repo_branch']} origin/#{magento[site]['repo_branch']}; git pull"
+    only_if { magento[site]['site_type'] == "magento" && magento[site]['repo_tag'] == false }
+  end
+  
+  execute 'checkout_branch' do
+    cwd "#{magento[site]['site_path']}/#{site}"
+    command "git fetch origin; git checkout -b #{magento[site]['repo_tag']} tags/#{magento[site]['repo_tag']}"
+    only_if { magento[site]['repo_tag'] }
+  end
+  
+  magento[site]['vhost'] = node['nt-deploy']['sites'][site].fetch('vhost', 'default')
+  magento[site]['db_name'] = node['nt-deploy']['sites'][site].fetch('db_name', "magento_#{site}")
+  magento[site]['db_user'] = node['nt-deploy']['sites'][site].fetch('db_user', site)
+  magento[site]['db_pwd'] = node['nt-deploy']['sites'][site].fetch('db_pwd', site)
+  magento[site]['db_host'] = node['nt-deploy']['sites'][site].fetch('db_host', node['nt-deploy']['default']['db_host'])
+  magento[site]['elb'] = node['nt-deploy']['sites'][site].fetch('elb', node['nt-deploy']['default']['elb'])
+  magento[site]['salt'] = node['nt-deploy']['sites'][site].fetch('salt', '')
+  magento[site]['cache_prefix'] = node['nt-deploy']['sites'][site].fetch('cache_prefix', "#{site}_")
+  magento[site]['sites_caches'] = node['nt-deploy']['sites'][site].fetch('sites_caches', [])
+  
+  magento[site]['site_dns'] = node['nt-deploy']['sites'][site].fetch('site_dns', 'www.example.net')
+  magento[site]['cron_key'] = node['nt-deploy']['sites'][site].fetch('cron_key', 'cron-key')
+  
+  directory "/media/ephemeral0/tmp/#{site}" do
+    owner 'apache'
+    group 'apache'
+    mode '0755'
+    action :create
+    recursive true
+  end
+  
+  template "#{magento[site]['site_path']}/#{site}/magento/app/etc/local.xml" do
+    source "local.xml.erb"
+    mode '0440'
+    owner 'apache'
+    group 'apache'
+    variables ({
+      :db_name => magento[site]['db_name'],
+      :db_user => magento[site]['db_user'],
+      :db_pwd => magento[site]['db_pwd'],
+      :db_host => magento[site]['db_host'],
+      :salt => magento[site]['salt']
+    })
+    only_if { magento[site]['site_type'] == "magento" }
+  end
+
+end
