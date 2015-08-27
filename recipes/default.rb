82ce87b7680bc7a43108b5cfabfa897e29476d6e
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2015-08-27 12:44:42 +0100

    Add Drupal vhost config file to default

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file '/etc/httpd/drupal_vhost.ini' do
+  source 'drupal_vhost.ini'
+  mode '0544'
+  owner 'apache'
+  group 'apache'
+end
+
+cookbook_file '/etc/httpd/drupal_htaccess.ini' do
+  source 'default_drupal.ini'
+  mode '0544'
+  owner 'apache'
+  group 'apache'
+end
