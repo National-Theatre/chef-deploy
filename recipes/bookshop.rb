9536928cd5df57ce3122e6c2beeebbe8eb843d0d
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2016-05-03 19:01:36 +0100

    break swap out

--- recipes/bookshop.rb
+++ recipes/bookshop.rb
@@ -20,30 +20,3 @@ selinux_policy_boolean 'httpd_use_fusefs' do
     notifies :restart,'service[httpd]', :delayed
 end
 
-if (::File.exists?("/swap.img") == false)
-  Chef::Log.info("Swapfile not found. Manually creating one of 512M for OOM safety")
-  execute "creating swapfile" do
-    command "/bin/dd if=/dev/zero of=/swap.img bs=1M count=512"
-    action :run
-    creates "/swap.img"
-  end
-
-  execute "formatting swapfile" do
-    command "/sbin/mkswap -L local /swap.img"
-    action :run
-  end
-
-  mount "none" do
-    device "/swap.img"
-    fstype "swap"
-    options [ "sw"]
-    dump 0
-    pass 0
-    action :enable
-  end
-
-  execute "mounting swapfile" do
-    command "/sbin/swapon -a"
-    action :run
-  end
-end
---
+++ recipes/sort_swap.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: nt-deploy
+# Recipe:: sort_swap
+#
+# Copyright 2016, National Theatre
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if (::File.exists?("/swap.img") == false)
+  Chef::Log.info("Swapfile not found. Manually creating one of 512M for OOM safety")
+  execute "creating swapfile" do
+    command "/bin/dd if=/dev/zero of=/swap.img bs=1M count=512"
+    action :run
+    creates "/swap.img"
+  end
+
+  execute "formatting swapfile" do
+    command "/sbin/mkswap -L local /swap.img"
+    action :run
+  end
+
+  mount "none" do
+    device "/swap.img"
+    fstype "swap"
+    options [ "sw"]
+    dump 0
+    pass 0
+    action :enable
+  end
+
+  execute "mounting swapfile" do
+    command "/sbin/swapon -a"
+    action :run
+  end
+end
