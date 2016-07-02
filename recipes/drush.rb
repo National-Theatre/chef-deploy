1f1598390e98db5fff0602b0e56c741049f73588
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2016-07-03 00:56:31 +0100

    get drush to work

--- recipes/drush.rb
+++ recipes/drush.rb
@@ -15,11 +15,29 @@ remote_file '/usr/bin/composer' do
   action :create
 end
 
+directory "/home/ec2-user/.config/composer" do
+  mode '0755'
+  owner 'ec2-user'
+  group 'ec2-user'
+  action :create
+  recursive true
+end
+template "/home/ec2-user/.config/composer/auth.json" do
+  source "auth.json.erb"
+  mode '0600'
+  owner 'ec2-user'
+  group 'ec2-user'
+  variables ({
+    :token => node['nt-deploy']['github']
+  })
+end
+
 execute 'get_drush' do
   command 'composer global require drush/drush'
   not_if { ::File.exists?("/home/ec2-user/.config/composer/vendor/bin/drush")}
   user 'ec2-user'
   group 'ec2-user'
+  environment ({"COMPOSER_HOME" => "/home/ec2-user/.config/composer"})
 end
 
 link '/usr/bin/drush' do
